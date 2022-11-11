ea0f8bafe4f0b9a5f4da95f4e6a1278eb62cf739
phung111 <men01225423493@gmail.com>
2022-11-11 14:38:04 +0700

    123

---
+++ BT+TH/7.Variable/BT_1.js
@@ -0,0 +1,26 @@
+// Bài 1: Viết ct tính điểm tb, tổng
+vatly = prompt('Nhập điểm Vật Lý: ')
+hoahoc = prompt('Nhập điểm Hoá Học: ')
+sinhhoc = prompt('Nhập điểm Sinh Học: ')
+
+let trungbinh = (vatly + hoahoc + sinhhoc)/3
+let tong = vatly + hoahoc + sinhhoc
+
+document.write('Điểm trung bình là:' +trungbinh)
+document.write('Điểm trung bình là:' +tong)
+
+// // Bài 2: Viết ct đổi độ C sang độ F
+// C = prompt('Nhập nhiệt độ C: ')
+// let F = 9*C/5 +32
+// document.write('Đổi sang nhiệt độ F là:' + C)
+
+// // Bài 3: Viết ct tính diện tích hình tròn
+// r = prompt('Nhập bán kính r: ')
+// let dientichhinhtron = π*r*r
+// document.write('Diện tích hình tròn là:' + dientichhinhtron)
+
+// // Bài 4: Viết ct tính chu vi hình tròn
+// let chuvihinhtron = π*2*r
+// document.write('Diện tích hình tròn là:' + chuvihinhtron)
+
+
