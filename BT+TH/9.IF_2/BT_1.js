d11ae3af17770586d0be3d63faf229982f3ad7a2
phung111 <men01225423493@gmail.com>
2022-11-14 20:16:18 +0700

    cpanhat

---
+++ BT+TH/9.IF_2/BT_1.js
@@ -0,0 +1,12 @@
+let cannang = 70;
+let chieucao = 1.75;
+let bmi = cannang / (chieucao * chieucao)
+
+if (bmi < 18)
+    console.log('Hơi gầy');
+else if (bmi < 25.0)
+    console.log('Bình thường');
+else if (bmi < 30.0)
+    console.log('Mập');
+else
+    console.log('Quá mập');
