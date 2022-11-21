b14d74a3ce83e1880a7782558c2e32ba66d8016c
phung111 <men01225423493@gmail.com>
2022-11-21 17:28:35 +0700

    3213123

---
+++ Practice and Exercise/13.Array_2/b.js
@@ -0,0 +1,8 @@
+
+a = -4.6
+console.log(a);
+console.log("ceil lên " + Math.ceil(a));
+console.log("floor xuống " + Math.floor(a));
+console.log("round gần " + Math.round(a));
+console.log("trunc thân " + Math.trunc(a));
+console.log("số nguyên " + parseInt(a));
